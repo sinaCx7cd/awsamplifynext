bc89a4f81e766cedff353e841552d7c803e33185
Oliwier Wolf <oliwier.wolf@ihsmarkit.com>
2021-11-29 08:38:50 +0100

    init

---
+++ pages/posts/[id].js
@@ -0,0 +1,35 @@
+import Layout from '../../components/layout'
+import { getPostData } from '../../lib/constposts'
+import Head from 'next/head'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.id}</title>
+      </Head>
+      <article>
+        <h1 >{postData.id}</h1>
+      </article>
+    </Layout>
+  )
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: 'blocking'
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id)
+  return {
+    props: {
+      postData
+    },
+    revalidate: 10
+  }
+}
